4a5316a6ea0f818fb66380c5e41eeae64cf91ab7
Joshua Franco <francoswim@yahoo.com>
2024-07-10 20:16:12 -0400

    Learning how to detect operating system

--- detecVM.c
+++ detecVM.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+#ifdef _WIN32
+    const char* os = "Windows";
+#elif __APPLE__
+    const char* os = "macos";
+#elif __linux__
+    const char* os = "Linux";
+#elif __unix__
+    const char* os = "POSIX";
+#else
+    const char* os = "Unknown";
+#endif
+
 int main(){
-    printf("Hello World\n");
+    printf("Operating System: %s\n", os);
     return 0;
 }
